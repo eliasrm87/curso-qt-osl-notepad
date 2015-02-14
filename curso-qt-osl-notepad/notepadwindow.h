ceb6f9ee527a53d5a063d3b489eede6672e8870b
ADRIAN <alu0100288216@siga.(none)>
2015-02-14 13:44:35 +0000

    Primer commit.

--- curso-qt-osl-notepad/notepadwindow.h
+++ curso-qt-osl-notepad/notepadwindow.h
@@ -12,10 +12,12 @@
 #include <QFontDialog>
 #include <QClipboard>
 #include <QKeySequence>
+#include <QMessageBox>
+#include <QToolBar>
 
 class NotepadWindow : public QMainWindow
 {
-    Q_OBJECT
+    Q_OBJECT  //directiva del compilador de QT.
 
 public:
     NotepadWindow(QWidget *parent = 0);
@@ -25,19 +27,28 @@ private slots:
     void alAbrir();
     void alGuardar();
     void alFuente();
+    void alSalir();
+    void alAcercaDe();
 
 private:
     QMenuBar*       mainMenu_;
     QMenu*          mnuArchivo_;
+    QMenu*          mnuAyuda_;
     QAction*        actArchivoAbrir_;
     QAction*        actArchivoGuardar_;
+    QAction*        actArchivoSalir_;
     QMenu*          mnuFormato_;
     QAction*        actFormatoFuente_;
     QMenu*          mnuEditar_;
     QAction*        actEditarCopiar_;
     QAction*        actEditarPegar_;
+    QAction*        actEditarCortar_;
+    QAction*        actEditarDeshacer_;
+    QAction*        actEditarRehacer_;
     QPlainTextEdit* txtEditor_;
     QClipboard *    portapapeles_;
+    QAction*        actAcercaDe_;
+    QToolBar*       mnuToolBar_;
 
 };
 
